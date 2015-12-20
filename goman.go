18896bf22c4a77d0d9550160bc3ee27b623eea19
Matt Davis <mattdavis9@gmail.com>
2015-12-19 22:22:04 -0800

    True enumerated macros

--- goman.go
+++ goman.go
@@ -8,7 +8,7 @@ import (
 	"log"
 	"os"
 	"regexp"
-    "strings"
+	"strings"
 )
 
 type Opt struct {
@@ -24,65 +24,94 @@ type ManPage struct {
 }
 
 type ParseError struct {
-    errmsg string
+	errmsg string
 }
 
 type Macro struct {
-    loc []int
-    mtype MacroType
+	loc   []int
+	mtype MacroType
 }
 
-type MacroType string
+type MacroType int
 
 const (
-  TP MacroType = "TP"
-  SH MacroType = "SH"
-  PP MacroType = "PP"
+	B MacroType = iota
+	IP
+	PP
+	SH
+	TP
 )
 
-func (pe *ParseError) Error() string {
-    return pe.errmsg
+var MacroTypes = map[string]MacroType {
+    "B":  B,
+    "IP": IP,
+    "PP": PP,
+    "SH": SH,
+    "TP": TP,
 }
 
-func (man *ManPage) nextMacro(offset int) (*Macro, *ParseError) {
-    re := regexp.MustCompilePOSIX(`^\.`)
-    if idx := re.FindStringIndex(man.data[offset:]); idx != nil {
-        index := []int{offset + idx[0], offset + idx[1]}
-        mt := MacroType("TP")
-        return &Macro{loc:index, mtype:mt} , nil
-    }
-    return nil, &ParseError{"Error locating next macro"}
+func (pe *ParseError) Error() string {
+	return pe.errmsg
+}
+
+func (man *ManPage) nextMacroOffset(offset int) *Macro {
+	re := regexp.MustCompilePOSIX(`^\.[A-Z]+ `)
+	if idx := re.FindStringIndex(man.data[offset:]); idx != nil {
+		index := []int{offset + idx[0], offset + idx[1]}
+        str := man.data[index[0]+1 : index[1]-1]
+		mt := MacroType(MacroTypes[str])
+		return &Macro{loc: index, mtype: mt}
+	}
+	return nil
+}
+
+func (man *ManPage) nextMacro(macro *Macro) *Macro {
+	return man.nextMacroOffset(macro.loc[1])
 }
 
 func (man *ManPage) findSection(name string) (int, *ParseError) {
-	re := regexp.MustCompilePOSIX(`^\.SH.*` + name)
+	re := regexp.MustCompilePOSIX(`^\.SH *` + name)
 	if idx := re.FindStringIndex(man.data); idx != nil {
-        return idx[1], nil
-    }
-    return -1, &ParseError{"Error locating section"}
+		return idx[1], nil
+	}
+	return -1, &ParseError{"Error locating section"}
 }
 
-func (man *ManPage) parseDesc() {
-	man.desc = "N/A"
-	if idx, err := man.findSection("DESCRIPTION"); err == nil {
+func stripMacros(str string) {
+    re := regexp.MustCompilePOSIX(`^\.[A-Z]+ *`)
+    str = re.ReplaceAllString(str, "")
+}
+
+func (m *ManPage) parseDesc() {
+	m.desc = "N/A"
+	if idx, err := m.findSection("DESCRIPTION"); err == nil {
 		start := idx
-        if macro, err := man.nextMacro(start); err == nil {
-			man.desc = man.data[start : macro.loc[1]]
+        var mc *Macro
+		for mc = m.nextMacroOffset(start); mc != nil; mc = m.nextMacro(mc) {
+			if mc.mtype == SH {
+				break
+			}
+		}
+        if mc == nil {
+            m.desc = m.data[start:]
+        } else {
+            m.desc = m.data[start:mc.loc[0]]
         }
+        stripMacros(m.desc)
 	}
 }
 
 func (man ManPage) String() string {
-	return fmt.Sprintf("Name: %s\n"+
+	return fmt.Sprintf("Name: %s\n" +
 		"Desc: %s\n", man.name, man.desc)
 }
 
 func (man *ManPage) parse(data string) {
-    // Remove carriage return
-    replace := strings.NewReplacer("\x0D", "")
+	// Remove carriage return
+	replace := strings.NewReplacer("\x0D", "")
 	man.data = replace.Replace(data)
 
-    // Parse all of the interesting parts
+	// Parse all of the interesting parts
 	man.parseDesc()
 	//man.parseOpts()
 }
